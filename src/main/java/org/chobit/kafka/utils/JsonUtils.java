b4b78963ee160dc2d70d96107c5acdeec8f12ffc
rui.zhang <rui.zhang@tendcloud.com>
2019-02-28 19:20:29 +0800

    alter json utils
    Change-Id: I092db56c2199e73c64ebe819db09dfbb78bc2236

--- src/main/java/org/chobit/kafka/utils/JsonUtils.java
+++ src/main/java/org/chobit/kafka/utils/JsonUtils.java
@@ -1,6 +1,9 @@
 package org.chobit.kafka.utils;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.chobit.kafka.exception.KafkaConfigException;
 
 import java.io.IOException;
@@ -8,6 +11,18 @@ import java.io.IOException;
 public abstract class JsonUtils {
 
     private static final ObjectMapper mapper = new ObjectMapper();
+    static {
+        mapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
+
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_DEFAULT);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_ABSENT);
+
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+    }
 
 
     public static <T> T fromJson(String json, Class<T> clazz) {
